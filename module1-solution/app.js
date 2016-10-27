dd4425245f9d6174908e9b742292499e5d09454f
marian27s <maryan27m@gmail.com>
2016-10-27 17:00:38 +0200

    Update app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,12 +8,14 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController ($scope) {
-  $scope.dishes = '';
-  $scope.ArrDihes = [];
   $scope.Check = function () {
     $scope.ArrDihes =  $scope.dishes.split(",");
+    $scope.DihesNumb = $scope.ArrDihes.length;
+    if (($scope.DihesNumb == 1) && ($scope.ArrDihes[0] == ""))  {$scope.Mesege = "Please enter data first" 
+	} else if($scope.DihesNumb <= 3)  {$scope.Mesege = "Enjoy!"
+	} else if($scope.DihesNumb > 3)  {$scope.Mesege = "Too much!"};
   };
-  return $scope.ArrDihes
+  return $scope.Mesege
 }
 
 })();
