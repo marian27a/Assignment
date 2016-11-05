9623cdc95a920d52af34ca9abc630e7f1b7a5e29
marian27s <maryan27m@gmail.com>
2016-11-05 19:07:06 +0100

    Update app.js

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -8,45 +8,44 @@ angular.module('ShoppingListCheckOff', [])
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 
-ToBuyController.$inject = ['ShoppingListService'];
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
   var itemDelete = this;
-
-  itemDelete.items = ShoppingListService.getItems();
+  itemDelete.items = ShoppingListCheckOffService.getItems();
+  itemDelete.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
 
 }
 
 
-
-
-
-
-
-
-AlreadyBoughtController.$inject = ['ShoppingListService'];
-  
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
   var alreadyBought = this;
 
-}  
+  alreadyBought.items = ShoppingListCheckOffService.getItemsAlrBoug();
 
-
-
-
-
-
-
-
+}
 
 
 function ShoppingListCheckOffService() {
   var service = this;
+ 
   var items = [{ name: "cookies", quantity: 10 }, { name: "Milk", quantity: '10 bottles'}, { name: "Sugar", quantity: '3'},{ name: "ice cream", quantity: 5},{ name: "Banana", quantity: 6}];
+  var boughtitems = [];
+
+  service.removeItem = function (itemIdex) {
+    boughtitems.push(items[itemIdex]);
+    items.splice(itemIdex, 1);
+  };
+
   service.getItems = function () {
     return items;
   };
+  service.getItemsAlrBoug = function () {
+    return boughtitems;
+  };
 }
-
-
 
 })();
