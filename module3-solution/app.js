337df12f79579efc354b46532368eef1298a0c26
marian27s <maryan27m@gmail.com>
2016-11-12 15:58:26 +0100

    Update app.js

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,1 +1,56 @@
+(function () {
+
+    'use strict';
+angular.module('NarrowItDownApp',[])
+.controller('NarrowItDownController',NarrowItDownController)
+.service('MenuSearchService',MenuSearchService)
+.constant('ApiBasePath',"https://davids-restaurant.herokuapp.com/");
+
+        NarrowItDownController.$inject=['MenuSearchService'];
+
+        function NarrowItDownController(MenuSearchService)
+        {
+            var menu = this;
+            var  menu_items = [];    
+           // var foundItems = [];           
+             
+            menu.searchItems = function () {           
+           menu_items =  MenuSearchService.getMatchedMenuItems(menu.search);
+                console.log("In function  " + menu.search);              
+                console.log("OK RESPONSE POSITIVE!");
+                console.log(menu_items);
+             };
+
+                    
+      }
+       
+        MenuSearchService.$inject = ['$http','ApiBasePath'];
+
+        function MenuSearchService($http,ApiBasePath)
+        {
+            var service = this; 
+            var foundItems = [];    
+            service.getMatchedMenuItems = function(searchTerm)     
+            {                    
+                console.log("Search Term : " + searchTerm);
+                var httpData = $http.get(ApiBasePath + "/menu_items.json")
+                    .then(function success(httpData)
+                    {
+                        console.log("SUCCESS");                 
+                        console.log("Status of the request: " + httpData.status); 
+                        console.log("Text of response " + httpData.statusText);                         
+                        foundItems = httpData.data;
+                        console.log(foundItems);               
+                    },
+                    function error(httpData)
+                    {
+                       
+                         console.log("FAIL");
+                    });        
+                return foundItems;               
+            };
+
+        }
+
+})();
 
